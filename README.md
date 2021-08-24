050b9c586c08518c5c3fcd9ef44aa9384437dcfa
Cody Winchester <cjwinchester@gmail.com>
2021-08-24 17:47:49 -0600

    first push

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# jni-intermediate-2021
+# JNI intermediate data course
 Files for an Aug. 2021 workshop with the Judith Neilson Institute.
---
+++ python/notebooks/myfile.py
@@ -0,0 +1,6 @@
+codys_dog = {
+    'name': 'Charlie',
+    'breed': '?',
+    'birthdate': '2017-04-20',
+    'picture': 'https://raw.githubusercontent.com/ireapps/cfj-2018/master/img/charlie.jpg'
+}
